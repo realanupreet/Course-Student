3663e7429ecf47f16e9f8a34c0eca619b023f26d
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-08-17 09:43:35 +0530

    fix: sidebar

--- src/Pages/AdminPanelBoilerplate.jsx
+++ src/Pages/AdminPanelBoilerplate.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import ReportElement from "../Components/ReportElement";
 import SideBarCourse from "../Components/SidebarCourses";
 import NavSubCustom from "../Components/NavSubCustom";
-const AdminPanelBoilerplate = ({ children, page, route, sidebar=true, subtitle }) => {
+import SidebarCollapsable from '../Components/SideBarCollapsable';
+const AdminPanelBoilerplate = ({ children, page, route, sidebar = true, subtitle }) => {
 
     return (
         <>
             <main className=" md:my-4 flex h-auto justify-center font-['Poppins'] overflow-hidden	">
 
                 <div className="hidden lg:block w-auto h-auto  md:block">
-                    <SideBarCourse title="Admin Panel" guy="yes" sidebar={sidebar} />
+                    <SidebarCollapsable />
+                    <SideBarCourse title="Admin Panel" guy="yes" sidebar={ sidebar } />
                 </div>
 
                 <div className="w-full mx-2 h-auto box-border md:block">
-                    <NavSubCustom subtitle={subtitle} page={page} route={route} />
+                    <NavSubCustom subtitle={ subtitle } page={ page } route={ route } />
                     { children }
 
                 </div>
